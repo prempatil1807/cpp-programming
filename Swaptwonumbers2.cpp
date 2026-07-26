c5b34ecc08851a46e50f930da7870da3532161eb
Prem Patil <patilprem1807@gmail.com>
2026-07-26 04:20:21 +0000

    chanes

---
+++ Swaptwonumbers2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    num1 = num1 + num2;
+    num2 = num1 - num2;
+    num1 = num1 - num2;
+
+    cout << "After swapping:" << endl;
+    cout << "First number = " << num1 << endl;
+    cout << "Second number = " << num2 << endl;
+
+    return 0;
+}
